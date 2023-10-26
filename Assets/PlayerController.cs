2d13fa68244761af7440ad1be6451386f0132bde
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2023-10-25 20:53:43 -0400

    Cleanup

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,53 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float maxMoveSpeed;
     private float moveSpeed;
     private float jumpHeight;
-    private BoxCollider2D bottomCollider;
+    private BoxCollider2D boxColl;
+    [SerializeField] LayerMask jumpableGround;
+    private float movementDirection;
 
-    void Start(){
+    private void Start(){
         rb = GetComponent<Rigidbody2D>();
-        bottomCollider = transform.GetChild(0).gameObject.GetComponent<BoxCollider2D>();
-        maxMoveSpeed = 10f;
-        moveSpeed = 7f;
+        boxColl = GetComponent<BoxCollider2D>();
+        moveSpeed = 10f;
         jumpHeight = 20f;
     }
 
 
-    void Update(){
-        if (rb.velocity.magnitude < maxMoveSpeed)
-        {
+    private void Update(){
+        movementDirection = Input.GetAxis("Horizontal");
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddForce(Vector2.right * moveSpeed);
-
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddForce(Vector2.left * moveSpeed);
-            }
-            
+        if (Input.GetKeyDown(KeyCode.R)){
+            transform.position = new Vector3(0f,0f,0f);
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddForce(Vector2.left, ForceMode2D.Impulse);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddForce(Vector2.right, ForceMode2D.Impulse);
-            }
-            if (bottomCollider) { 
-            }
-            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(movementDirection * moveSpeed, rb.velocity.y);
         }
+        
+        if (Input.GetKeyDown(KeyCode.W) && CanJump())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    
     }
+ 
+    private bool CanJump(){
+        return Physics2D.BoxCast(boxColl.bounds.center, boxColl.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+
+
 }
