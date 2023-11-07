6a15d46910e1fe224025ebcaa22747eee81b0cfa
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2023-11-07 17:43:39 -0500

    Key Colours

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Collectable : MonoBehaviour
+public class Collectable : MonoBehaviour
 {
     public float NextTimePickup;
 
-    protected abstract void OnTriggerEnter2D(UnityEngine.Collider2D collision);
+    protected virtual void Start(){
+        NextTimePickup = Time.time;
+    }
+
+    protected virtual void OnTriggerEnter2D(UnityEngine.Collider2D other){
+        if ((other.gameObject.GetComponent<FireBoy>() != null || other.gameObject.GetComponent<WaterGirl>() != null) && Time.time > NextTimePickup)
+        {
+            if (other.gameObject.GetComponent<Character>().HeldItem != null)
+            {
+                other.gameObject.GetComponent<Character>().Drop();
+            }
+            other.gameObject.GetComponent<Character>().HeldItem = this;
+            transform.position = other.gameObject.GetComponent<Character>().Hand.position;
+            transform.SetParent(other.gameObject.GetComponent<Character>().Hand);
+        }
+    }
 }
--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 
+public enum Colors{
+    Red,
+    Blue
+}
 public class Key : Collectable
 {
-    private void Start(){
-        NextTimePickup = Time.time;
-    }
-    protected override void OnTriggerEnter2D(UnityEngine.Collider2D other)
-    {
-        if ((other.gameObject.GetComponent<FireBoy>() != null || other.gameObject.GetComponent<WaterGirl>() != null) && Time.time > NextTimePickup) {
-            if (other.gameObject.GetComponent<Character>().HeldItem != null){
+    public Colors _color;
+
+    protected override void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.GetComponent<FireBoy>() != null && _color == Colors.Red && Time.time > NextTimePickup || other.gameObject.GetComponent<WaterGirl>() != null && _color == Colors.Blue && Time.time > NextTimePickup){
+            if (other.gameObject.GetComponent<Character>().HeldItem != null)
+            {
                 other.gameObject.GetComponent<Character>().Drop();
             }
             other.gameObject.GetComponent<Character>().HeldItem = this;
             transform.position = other.gameObject.GetComponent<Character>().Hand.position;
             transform.SetParent(other.gameObject.GetComponent<Character>().Hand);
-
         }
     }
+
 }
