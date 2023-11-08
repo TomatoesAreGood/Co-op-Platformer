8459c1fc017200d57e0db8cc38c4ae31875d9e36
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2023-11-07 21:55:21 -0500

    End Screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
     public static FireBoy FireBoy;
     public static WaterGirl WaterGirl;
-    [SerializeField] IntToText CoinsText;
-    [SerializeField] IntToText TimeText;
-    [SerializeField] LevelExit LevelExit;
     public static int Coins;
     public static float Timer;
     public static Checkpoint Checkpoint;
+    [SerializeField] IntToText CoinsText;
+    [SerializeField] IntToText TimeText;
+    [SerializeField] LevelExit LevelExit;
+    [SerializeField] GameObject EndScreen;
+    private int _numTrophies;
 
 
     private void Start()
     {
+        _numTrophies = 0;
         Timer = 120;
         Coins = 0;
         Checkpoint = null;
@@ -27,21 +31,37 @@ public class GameManager : MonoBehaviour
     private void Update()
     {
         if (FireBoy.BoxColl.IsTouching(LevelExit.BoxColl)){
-            if(Input.GetKey(KeyCode.E) && !LevelExit.IsLocked){
-                FireBoy.ExitLevel = true;
-                FireBoy.gameObject.SetActive(false);
+            if (!LevelExit.IsLocked){
+                if(Input.GetKey(KeyCode.E)){
+                    FireBoy.ExitLevel = true;
+                    FireBoy.gameObject.SetActive(false);
+                }   
+                if (FireBoy.HeldItem is Trophy){
+                    _numTrophies++;
+                    Destroy(FireBoy.HeldItem.gameObject);
+                    FireBoy.HeldItem = null;
+                }
             }
+           
         }
 
         if (WaterGirl.BoxColl.IsTouching(LevelExit.BoxColl)){
-            if(Input.GetKey(KeyCode.RightShift) && !LevelExit.IsLocked){
-                WaterGirl.ExitLevel = true;
-                WaterGirl.gameObject.SetActive(false);
+            if (!LevelExit.IsLocked){
+                if(Input.GetKey(KeyCode.RightShift)){
+                    WaterGirl.ExitLevel = true;
+                    WaterGirl.gameObject.SetActive(false);
+                }
+                if (WaterGirl.HeldItem is Trophy){
+                    _numTrophies++;
+                    Destroy(WaterGirl.HeldItem.gameObject);
+                    WaterGirl.HeldItem = null;
+                }
             }
         }
 
         if (WaterGirl.ExitLevel && FireBoy.ExitLevel){
-            //load end screen
+            LoadEndScreen();
+            WaterGirl.ExitLevel = false;
         }
 
         if (!FireBoy.IsAlive || !WaterGirl.IsAlive){
@@ -56,7 +76,8 @@ public class GameManager : MonoBehaviour
         }
 
         if (Input.GetKeyDown(KeyCode.R)){
-            SceneManager.LoadScene("GamePlay");
+            LoadEndScreen();
+            //SceneManager.LoadScene("GamePlay");
         }
 
         Timer -= Time.deltaTime;
@@ -65,5 +86,42 @@ public class GameManager : MonoBehaviour
         }
         TimeText.UpdateText((int)Timer);
         CoinsText.UpdateText(Coins);
+    }
+
+    private void LoadEndScreen(){
+        GameObject star1 = EndScreen.transform.GetChild(0).gameObject;
+        GameObject star2 = EndScreen.transform.GetChild(1).gameObject;
+        GameObject star3 = EndScreen.transform.GetChild(2).gameObject;
+
+        EndScreen.transform.GetChild(3).GetComponent<TextMesh>().text = "Coins: " + Coins;
+        EndScreen.transform.GetChild(4).GetComponent<TextMesh>().text = "Trophies: " + _numTrophies;
+        int score = Coins + _numTrophies*50;
+        EndScreen.transform.GetChild(5).GetComponent<TextMesh>().text = "Score: " + score;
+
+        if (score < 50){
+            star1.SetActive(true);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        }else if (score < 150){
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(false);
+        }else{
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(true);
+        }
+        StartCoroutine(StartEndScreen());
+    }
+
+    private IEnumerator StartEndScreen(){
+        yield return new WaitForSeconds(1);
+        float startingPos = -24.06f;
+        float endPos = 12.33f;
+        for (float i = 0; i < 1; i += 0.01f){
+            EndScreen.transform.position = new Vector3(Mathf.Lerp(startingPos, endPos, i), -4.49f,transform.position.z);
+            yield return new WaitForSeconds(0.02f);
+        }
+
     }
 }
